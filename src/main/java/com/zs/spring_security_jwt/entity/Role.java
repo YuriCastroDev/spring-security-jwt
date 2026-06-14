94a9eb0799649050a1684d16ed2934d44f5261c6
Yuri Castro <yuri.castro.dev@gmail.com>
2026-06-14 12:01:04 -0300

    chore: setup project with postgresql and define user domain

---
+++ src/main/java/com/zs/spring_security_jwt/entity/Role.java
@@ -0,0 +1,7 @@
+package com.zs.spring_security_jwt.entity;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MODERATOR
+}
---
+++ src/main/java/com/zs/spring_security_jwt/entity/User.java
@@ -0,0 +1,68 @@
+package com.zs.spring_security_jwt.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean enabled;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.enabled = true;
+        if (this.role == null) this.role = Role.ROLE_USER;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return enabled; }
+}
